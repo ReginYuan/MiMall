5587d59f738ad8194e26663189fc6ecb28d40075
ReginYuan <reginyuan@gmail.com>
2020-07-06 16:47:47 +0800

    4-3 跨域-接口代理

--- src/vue.config.js
+++ src/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
                 target: 'https://www.imooc.com',
                 changeOringin: true, //是否将主机头的原点更改为目标的url地址
                 pathRewrite: { //转发代理
-                    '/api': '/api'
+                    '/api': ''
                 }
             }
         }
