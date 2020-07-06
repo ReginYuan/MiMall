e7fb8a7a464aa12a19dd6cd66a4602c6540779e1
ReginYuan <reginyuan@gmail.com>
2020-07-06 16:27:51 +0800

    4-3 跨域-接口代理

---
+++ src/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'https://www.imooc.com',
+                changeOringin: true, //是否将主机头的原点更改为目标的url地址
+                pathRewrite: { //转发代理
+                    '/api': '/api'
+                }
+            }
+        }
+
+    }
+
+}
