df1756df49db94ec25487115ca0f6cf693c39f8d
ReginYuan <reginyuan@gmail.com>
2020-07-07 10:43:21 +0800

    4-5 目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+    <router-view></router-view>
     <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld";
-// import axios from "axios";
-import jsonp from "jsonp";
-
 export default {
   name: "app",
   components: {
     // eslint-disable-next-line vue/no-unused-components
-    HelloWorld,
   },
   data() {
-    return {
-      age: 30,
-    };
+    return {};
   },
 
-  mounted() {
-    // axios发送get请求
-    // let url =
-    //   "https://www.imooc.com/course/list";
-    // axios.get(url).then(() => {});
-
-    // jsonp发送请求  它属于js
-    let url = "/api/course/list";
-    jsonp(url, (err, res) => {
-      let result = res;
-      this.data = result;
-    });
-  },
+  mounted() {}
 };
 </script>
 >
