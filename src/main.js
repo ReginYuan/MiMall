7033c393ce7f5d9b83764ed9bf4eb2e6b7cb63c7
ReginYuan <reginyuan@gmail.com>
2020-07-15 06:31:58 +0800

    请求本地 json mock

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import axios from './axios';
-import VueAxios from './vue-axios';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import store from './store';
-import env from './env';
+// import env from './env';
 
 // 接口代理 根据前端的跨域方式做调整
-axios.default.baseURL = '/api';
+// axios.default.baseURL = '/api';
 axios.default.timeout = 8000;
 // 根据环境变量获取不同的请求地址
-axios.default.baseURL = env.baseURL;
+// axios.default.baseURL = env.baseURL;
 //interceptors 拦截 response
 axios.interceptors.response.use(function(response) {
     let res = response.data;
