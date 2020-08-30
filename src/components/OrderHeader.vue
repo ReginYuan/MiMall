2771955ff66d6c1e2ab0a3c3d491c1242b75e95f
ReginYuan <reginyuan@gmail.com>
2020-08-30 17:39:48 +0800

    10-2 购物车页面

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -3,34 +3,36 @@
 */
 
 <template>
-  <div class="order-header">
+<div class="order-header">
     <div class="container clearfix">
-      <div class="header-logo">
-        <a href="/#/index"></a>
-      </div>
-      <div class="title">
-        <h2>
-          {{title}}
-          <slot name="tip"></slot>
-        </h2>
-      </div>
-      <div class="username">
-        <a href="javaScript:;">{{username}}</a>
-      </div>
+        <div class="header-logo">
+            <a href="/#/index"></a>
+        </div>
+        <div class="title">
+            <h2>
+                {{title}}
+                <slot name="tip"></slot>
+            </h2>
+        </div>
+        <div class="username">
+            <a href="javaScript:;">{{username}}</a>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import {
+    mapState
+} from "vuex";
 export default {
-  name: "order-header",
-  props: {
-    title: String,
-  },
-  computed: {
-    ...mapState(["username"]),
-  },
+    name: "order-header",
+    props: {
+        title: String,
+    },
+    computed: {
+        ...mapState(["username"]),
+    },
 };
 </script>
 
@@ -40,38 +42,43 @@ export default {
 @import "./../assets/scss/base.scss";
 
 .order-header {
-  padding: 30px 0;
+    padding: 30px 0;
 
-  .header-logo {
-    float: left;
-  }
-  .title,
-  .username {
-    display: inline-block;
-    height: 55px;
-    line-height: 55px;
-  }
-  .title {
-    float: left;
-    margin-left: 54px;
-    h2 {
-      font-size: 28px;
-      color: #333333;
+    .header-logo {
+        float: left;
     }
-    span {
-      font-size: 14px;
-      margin-left: 17px;
-      color: #999999;
-      font-weight: bold;
-    }
-  }
 
-  .username {
-    float: right;
-    a {
-      color: #666666;
-      font-size: 16px;
+    .title,
+    .username {
+        display: inline-block;
+        height: 55px;
+        line-height: 55px;
     }
-  }
+
+    .title {
+        float: left;
+        margin-left: 54px;
+
+        h2 {
+            font-size: 28px;
+            color: #333333;
+        }
+
+        span {
+            font-size: 14px;
+            margin-left: 17px;
+            color: #999999;
+            font-weight: 200;
+        }
+    }
+
+    .username {
+        float: right;
+
+        a {
+            color: #666666;
+            font-size: 16px;
+        }
+    }
 }
 </style>
